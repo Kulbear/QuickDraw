0671eae120427248bb0e8dcd322905b68156e383
Kulbear <jiyang.py@gmail.com>
2018-11-15 22:16:15 -0700

    Update the default training config

--- quick_draw/configs.py
+++ quick_draw/configs.py
@@ -1,18 +1,19 @@
 WIKI_URL = 'https://en.wikipedia.org/wiki/List_of_sovereign_states_and_dependent_territories_by_continent_(data_file)'
 
-DP_DIR = '../input/shuffle-csvs/'
-INPUT_DIR = '../input/quickdraw-doodle-recognition/'
+DP_DIR = './input/shuffle-csvs/'
+INPUT_DIR = './input/quickdraw-doodle-recognition/'
 
 COUNTRY_INFO_FILE = 'property.pkl'
+CATEGORY_LIST = 'categories.pkl'
 
 NUM_CSV = 100
 STEPS = 500
-EPOCHS = 160
+EPOCHS = 400
 IMG_SIZE = 128
 RAW_IMG_SIZE = 256
 NUM_CLASS = 340
 BATCH_SIZE = 128
 WARM_START = False
 BASE_LR = 0.0002
-CYCLE_EPOCHS = 40
-RANDOM_SEED = 1987
+CYCLE_EPOCHS = 50
+RANDOM_SEED = 666
